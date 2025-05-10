96b784cbe6ba9d4f99be213df935852abb5e566a
analayze <annalisa@antonioli.dev>
2025-05-10 15:36:29 +0200

    reorganized content

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,8 +2,10 @@ const defaultResult = 0
 let currentResult = defaultResult
 
 function add() {
-  currentResult = currentResult + userInput.value
-  outputResult(currentResult, '')
+  const enteredNumber = userInput.value
+  const calcDescription = `${currentResult} + ${enteredNumber}`
+  currentResult = currentResult + enteredNumber
+  outputResult(currentResult, calcDescription)
 }
 
 addBtn.addEventListener('click', add)
