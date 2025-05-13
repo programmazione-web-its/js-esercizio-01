e9eb641cb8552afbc4af45e9d88c51a206367269
analayze <annalisa@antonioli.dev>
2025-05-13 15:32:42 +0200

    esercizio finito

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 const task3Item = document.getElementById('task-3')
+
+function alertVuoto() {
+  alert('Ciao!')
+}
+
+function alertMessage(message) {
+  alert(message)
+}
+
+// alertVuoto()
+// alertMessage('Ciao a te!')
+function terza(st1, st2, st3) {
+  return st1 + ' ' + st2 + ' ' + st3
+}
+
+task3Item.addEventListener('click', alertVuoto)
+
+alert(terza('a', 'b', 'c'))
